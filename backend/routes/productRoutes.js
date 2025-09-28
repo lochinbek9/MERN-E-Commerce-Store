15971a5519b483631e9cc55f6f4265918eb747e3
Lochinbek Dehkonov <103257271+lochinbek9@users.noreply.github.com>
2025-09-28 20:07:33 +0500

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,7 +3,7 @@ import formidable from "express-formidable";
 
 const router = express.Router();
 
-// Controllers
+
 import {
   addProduct,
   updateProductDetails,
@@ -17,41 +17,31 @@ import {
   filterProducts,
 } from "../controllers/productController.js";
 
-// Middlewares
+
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 import checkId from "../middlewares/checkId.js";
 
-// @desc    Get products list or search
-// @route   GET /api/products
 router
   .route("/")
   .get(fetchProducts) // /api/products?keyword=...
   .post(authenticate, authorizeAdmin, formidable(), addProduct); // Add new product
 
-// @desc    Get all products (admin)
-// @route   GET /api/products/allproducts
+
 router.get("/allproducts", authenticate, authorizeAdmin, fetchAllProducts);
 
-// @desc    Filtered products (for search/filtering)
-// @route   POST /api/products/filtered-products
+
 router.post("/filtered-products", filterProducts);
 
-// @desc    Get top-rated products
-// @route   GET /api/products/top
+
 router.get("/top", fetchTopProducts);
 
-// @desc    Get new arrival products
-// @route   GET /api/products/new
+
 router.get("/new", fetchNewProducts);
 
-// @desc    Product reviews
-// @route   POST /api/products/:id/reviews
+
 router.post("/:id/reviews", authenticate, checkId, addProductReview);
 
-// @desc    Single product (view, update, delete)
-// @route   GET /api/products/:id
-//          PUT /api/products/:id
-//          DELETE /api/products/:id
+
 router
   .route("/:id")
   .get(checkId, fetchProductById)
